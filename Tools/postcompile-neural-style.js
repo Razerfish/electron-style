b1ba5b04f76c91fbbc2fc561f97f9dee4e60ec18
Alec Wilson <alec.wilson@baxter-academy.org>
2018-12-21 13:41:36 -0500

    Update postcompile-neural-style.js
    Updated paths and added error handling.

--- Tools/postcompile-neural-style.js
+++ Tools/postcompile-neural-style.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 const rimraf = require('rimraf');
 
-rimraf.sync("Assets\\Executables\\build");
+try {
+    rimraf.sync("Assets\\bin\\build");
+} catch (e) {
+    console.error(e);
+    console.error("Could not remove build folder");
+}
 
-fs.unlinkSync("neural_style.spec");
+try {
+    fs.unlinkSync("neural_style.spec");
+} catch (e) {
+    console.error(e);
+    console.error("Could not remove neural_style.spec");
+}
 
 if (!fs.existsSync("Assets\\Executables\\build") && !fs.existsSync("neural_style.spec")) {
     process.exit(0);
