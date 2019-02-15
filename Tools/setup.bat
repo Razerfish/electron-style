4aa2dedb0887ead09fd948bfa3106dbf09efc62c
Alec Wilson <alec.wilson@baxter-academy.org>
2019-02-15 09:53:00 -0500

    Create setup.bat

---
+++ Tools/setup.bat
@@ -0,0 +1,18 @@
+@echo off
+
+for /f "delims=" %%i in ('python -V') do set python=%%i
+set python=%python:~0,6%
+
+if /I %python% == Python (
+    if EXIST env\\pyvenv.cfg (
+        echo Python virtual environment already exists, if there's something wrong with it delete it and run setup again.
+    ) else (
+        python -m venv env
+        env\\Scripts\\activate.bat
+        python -m pip install --upgrade pip==19.0.2
+        pip install --upgrade setuptools
+        pip install -r requirements.txt
+    )
+) else (
+    echo "Python not found. Is it installed and added to PATH?"
+)
