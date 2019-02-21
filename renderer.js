219b09505bc71e55ec4a5507cd0e77bcd51aa61f
Alec Wilson <alec.wilson@baxter-academy.org>
2019-02-20 23:23:09 -0600

    Update renderer.js
    Completly restructured the contents of renderer.js.

--- renderer.js
+++ renderer.js
@@ -1,17 +1,53 @@
-const electron = require('electron');
-
-const {ipcRenderer, remote} = electron;
-
-function load_train_window() {
-    console.log("Training requested.");
-    remote.getCurrentWindow().loadFile('./Assets/HTML/train.html');
+function isDev() {
+    // Checks and returns if the main process is running in a development environment or not.
+    return process.mainModule.filename.indexOf('app.asar') === -1;
 }
 
-function load_stylize_window() {
-    console.log("Stylize requested.");
-    remote.getCurrentWindow().loadFile('./Assets/HTML/stylize.html');
+function create_neural_style(args) {
+    /* Creates a subprocess and passes the given args to it.
+    Uses a different binary depending on whether or not it is running in a development environment. */
+    const execFile = require('child_process').execFile;
+
+    if (isDev()) {
+        return execFile("./Assets/bin/neural_style/neural_style.exe", [JSON.stringify(args)]);
+    } else {
+        return execFile("./resources/app.asar.unpacked/Assets/bin/neural_style/neural_style.exe", [JSON.stringify(args)]);
+    }
 }
 
-document.querySelector('#train_button').addEventListener('click', load_train_window);
+function isCUDA() {
+    const execFile = require("child_process").execFile;
+    
+    let get_CUDA;
+    if (isDev()) {
+        get_CUDA = execFile("./Assets/bin/neural_style/check_cuda.exe");
+    } else {
+        get_CUDA = execFile("./resources/app.asar.unpacked/Assets/bin/neural_style/check_cuda.exe");
+    }
+    
+    get_CUDA.stdout.on('data', (data) => {
+        data = JSON.parse(data.toString());
+        if (data["cuda_available"] === "True") {
+            console.log("CUDA is available");
+            return true;
+        } else {
+            console.log("CUDA is not available");
+            return false;
+        }
+    });
+}
 
-document.querySelector('#stylize_button').addEventListener('click', load_stylize_window);
+function parse_data(data) {
+    if (data["type"] === "status_update") {
+        return "Status: " + data["status"];
+    } else if (data["type"] === "dataset_info") {
+        global.dataset = data["dataset_length"];
+        return "Dataset length: " + dataset;
+    } else if (data["type"] === "training_progress") {
+        return "Progress: " + data["progress"] + "/" + dataset + " " + data["percent"] + "%";
+    } else if (data["type"] === "log") {
+        console.log(data["message"]);
+    } else {
+        return "Unknown type: " + data["type"];
+    }
+}
