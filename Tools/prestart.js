e361bab2de466d4da9a1824ac468e88c46eeb0e3
Alec Wilson <alec.wilson@baxter-academy.org>
2019-03-13 18:20:53 -0400

    Fix typo in prestart.js

--- Tools/prestart.js
+++ Tools/prestart.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 if (fs.existsSync("Assets\\bin\\neural_style\\neural_style.exe")) {
     process.exit(0);
 } else {
-    console.error("neural_style.exe not found, recompile by running 'npm run compile-neural-style'");
+    console.error("neural_style.exe not found, recompile by running 'npm run compile'");
     process.exit(1);
 }
