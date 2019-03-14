27e69cd7ffd28b793efca7abbef361f786843364
Alec Wilson <alec.wilson@baxter-academy.org>
2019-03-14 10:17:42 -0400

    Remove and Replace
    Removed all files and replaced them with a blank electron-forge project.

--- Tools/prestart.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-if (fs.existsSync("Assets\\bin\\neural_style\\neural_style.exe")) {
-    process.exit(0);
-} else {
-    console.error("neural_style.exe not found, recompile by running 'npm run compile'");
-    process.exit(1);
-}
