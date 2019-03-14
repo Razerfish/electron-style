5b84ef75ec15b4a753a4d7b348550e982af4f584
Alec Wilson <alec.wilson@baxter-academy.org>
2019-03-14 11:59:41 -0400

    Add Tools Scripts
    Added Tools scripts and updated pathing.

---
+++ Tools/prepackage.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const exec = require('child_process').exec;
+
+if (!fs.existsSync("src\\bin\\neural_style\\neural_style.exe")) {
+    console.log("neural_style.exe not found, recompiling...");
+    compile_process = exec('npm run compile');
+
+    compile_process.stderr.on('data', (data) => {
+        console.error(data.toString());
+    });
+
+    compile_process.on('exit', (code) => {
+        if (code != 0) {
+            process.exit(1);
+        } else {
+            process.exit(0);
+        }
+    });
+} else {
+    process.exit(0);
+}
---
+++ Tools/prestart.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+if (fs.existsSync("src\\bin\\neural_style\\neural_style.exe")) {
+    process.exit(0);
+} else {
+    console.error("neural_style.exe not found, recompile by running 'npm run compile'");
+    process.exit(1);
+}
