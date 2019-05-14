f8cb08ae5d4617ce9bd876d88989e8a73688c2b2
Alec Wilson <alec.wilson@baxter-academy.org>
2019-05-14 14:03:51 -0400

    Update stage logging formatting

--- Assets/Scripts/neural_style/neural_style.py
+++ Assets/Scripts/neural_style/neural_style.py
@@ -23,10 +23,7 @@ from neural_style.vgg import Vgg16
 import argparse
 
 def check_paths(args):
-    status_update({
-        "task": "path_validation",
-        "action": "start"
-    })
+    status_update({"task": "path_validation", "action": "start"})
 
     try:
         if not os.path.exists(args.save_model_dir):
@@ -35,46 +32,25 @@ def check_paths(args):
             os.makedirs(args.checkpoint_model_dir)
     except OSError as e:
         sys.exit(e)
-    status_update({
-        "task": "path_validation",
-        "action": "finish"
-    })
+    status_update({"task": "path_validation", "action": "finish"})
 
 def train(args):
-    status_update({
-        "task": "training_setup",
-        "action": "start"
-    })
+    status_update({"task": "training_setup", "action": "start"})
 
-    status_update({
-        "task": "torch_device_creation",
-        "action": "start"
-    })
+    status_update({"task": "torch_device_creation", "action": "start"})
 
     device = torch.device("cuda" if args.cuda else "cpu")
 
-    status_update({
-        "task": "torch_device_creation",
-        "action": "finish"
-    })
+    status_update({"task": "torch_device_creation", "action": "finish"})
 
-    status_update({
-        "task": "seed_creation",
-        "action": "start"
-    })
+    status_update({"task": "seed_creation", "action": "start"})
 
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
-    status_update({
-        "task": "seed_creation",
-        "action": "finish",
-    })
+    status_update({"task": "seed_creation", "action": "finish",})
 
-    status_update({
-        "task": "transform_creation",
-        "action": "start"
-    })
+    status_update({"task": "transform_creation", "action": "start"})
 
     transform = transforms.Compose([
         transforms.Resize(args.image_size),
@@ -83,166 +59,82 @@ def train(args):
         transforms.Lambda(lambda x: x.mul(255))
     ])
     
-    status_update({
-        "task": "transform_creation",
-        "action": "finish"
-    })
+    status_update({"task": "transform_creation", "action": "finish"})
 
-    status_update({
-        "task": "loading_dataset",
-        "action": "start"
-    })
+    status_update({"task": "loading_dataset", "action": "start"})
 
-    status_update({
-        "task": "dataset_file_loading",
-        "action": "start"
-    })
+    status_update({"task": "dataset_file_loading", "action": "start"})
 
     train_dataset = datasets.ImageFolder(args.dataset, transform)
-    log(json.dumps({
-        "type": "dataset_length",
-        "data": len(train_dataset) * args.epochs
-    }))
+    log(json.dumps({"type": "dataset_length", "data": len(train_dataset) * args.epochs}))
 
-    status_update({
-        "task": "dataset_file_loading",
-        "action": "finish"
-    })
+    status_update({"task": "dataset_file_loading", "action": "finish"})
 
-    status_update({
-        "task": "trainloader_creation",
-        "action": "start"
-    })
+    status_update({"task": "trainloader_creation", "action": "start"})
 
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size)
 
-    status_update({
-        "task": "trainloader_creation",
-        "action": "finish"
-    })
+    status_update({"task": "trainloader_creation", "action": "finish"})
 
-    status_update({
-        "task": "loading_dataset",
-        "action": "finish"
-    })
+    status_update({"task": "loading_dataset", "action": "finish"})
 
-    status_update({
-        "task": "transformer_creation",
-        "action": "start"
-    })
+    status_update({"task": "transformer_creation", "action": "start"})
 
     transformer = TransformerNet().to(device)
 
-    status_update({
-        "task": "transformer_creation",
-        "action": "finish"
-    })
+    status_update({"task": "transformer_creation", "action": "finish"})
 
-    status_update({
-        "task": "optimizer_creation",
-        "action": "start"
-    })
+    status_update({"task": "optimizer_creation", "action": "start"})
 
     optimizer = Adam(transformer.parameters(), args.lr)
 
-    status_update({
-        "task": "optimizer_creation",
-        "action": "finish"
-    })
+    status_update({"task": "optimizer_creation", "action": "finish"})
 
 
-    status_update({
-        "task": "mse_loss_creation",
-        "action": "start"
-    })
+    status_update({"task": "mse_loss_creation", "action": "start"})
 
     mse_loss = torch.nn.MSELoss()
 
-    status_update({
-        "task": "mse_loss_creation",
-        "action": "finish"
-    })
+    status_update({"task": "mse_loss_creation", "action": "finish"})
 
-    status_update({
-        "task": "vgg_creation",
-        "action": "start"
-    })
+    status_update({"task": "vgg_creation", "action": "start"})
 
     vgg = Vgg16(requires_grad=False).to(device)
 
-    status_update({
-        "task": "vgg_creation",
-        "action": "finish"
-    })
+    status_update({"task": "vgg_creation", "action": "finish"})
 
-    status_update({
-        "task": "style_transform_creation",
-        "action": "start"
-    })
+    status_update({"task": "style_transform_creation", "action": "start"})
 
     style_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Lambda(lambda x: x.mul(255))
     ])
 
-    status_update({
-        "task": "style_transform_creation",
-        "action": "finish"
-    })
+    status_update({"task": "style_transform_creation", "action": "finish"})
 
-    status_update({
-        "task": "style_image_loading",
-        "action": "start"
-    })
+    status_update({"task": "style_image_loading", "action": "start"})
     style = utils.load_image(args.style_image, size=args.style_size)
-    status_update({
-        "task": "style_image_loading",
-        "action": "finish"
-    })
+    status_update({"task": "style_image_loading", "action": "finish"})
 
-    status_update({
-        "task": "prepping_style_image",
-        "action": "start"
-    })
+    status_update({"task": "prepping_style_image", "action": "start"})
     style = style_transform(style)
     style = style.repeat(args.batch_size, 1, 1, 1).to(device)
-    status_update({
-        "task": "prepping_style_image",
-        "action": "finish"
-    })
+    status_update({"task": "prepping_style_image", "action": "finish"})
 
-    status_update({
-        "task": "feature_style_creation",
-        "action": "start"
-    })
+    status_update({"task": "feature_style_creation", "action": "start"})
     features_style = vgg(utils.normalize_batch(style))
-    status_update({
-        "task": "feature_style_creation",
-        "action": "finish"
-    })
+    status_update({"task": "feature_style_creation", "action": "finish"})
     
-    status_update({
-        "task": "gram_style_creation",
-        "action": "start"
-    })
+    status_update({"task": "gram_style_creation", "action": "start"})
     gram_style = [utils.gram_matrix(y) for y in features_style]
-    status_update({
-        "task": "gram_style_creation",
-        "action": "finish"
-    })
+    status_update({"task": "gram_style_creation", "action": "finish"})
 
     progress_count = 0
 
-    status_update({
-        "task": "training_setup",
-        "action": "finish"
-    })
+    status_update({"task": "training_setup", "action": "finish"})
 
 
-    status_update({
-        "task": "training",
-        "action": "start"
-    })
+    status_update({"task": "training", "action": "start"})
 
     for e in range(args.epochs):
         transformer.train()
@@ -299,31 +191,16 @@ def train(args):
                 torch.save(transformer.state_dict(), ckpt_model_path)
                 transformer.to(device).train()
 
-    status_update({
-        "task": "training",
-        "action": "finish"
-    })
+    status_update({"task": "training", "action": "finish"})
 
 
-    status_update({
-        "task": "saving_model",
-        "action": "start"
-    })
+    status_update({"task": "saving_model", "action": "start"})
     # save model
-    status_update({
-        "task": "evaluating_model",
-        "action": "start"
-    })
+    status_update({"task": "evaluating_model", "action": "start"})
     transformer.eval().cpu()
-    status_update({
-        "task": "evaluating_model",
-        "action": "finish"
-    })
+    status_update({"task": "evaluating_model", "action": "finish"})
 
-    status_update({
-        "task": "finalizing_model_path",
-        "action": "start"
-    })
+    status_update({"task": "finalizing_model_path", "action": "start"})
     if args.name is None:
         save_model_filename = str(os.path.normpath(os.path.basename(args.style_image))
         [0:int(os.path.normpath(os.path.basename(args.style_image)).rfind("."))]) + ".pth"
@@ -331,84 +208,42 @@ def train(args):
         save_model_filename = str(args.name + ".pth")
     
     save_model_path = os.path.join(args.save_model_dir, save_model_filename)
-    status_update({
-        "task": "finalizing_model_path",
-        "action": "finish"
-    })
+    status_update({"task": "finalizing_model_path", "action": "finish"})
     
-    status_update({
-        "task": "saving_model_to_file",
-        "action": "start"
-    })
+    status_update({"task": "saving_model_to_file", "action": "start"})
     torch.save(transformer.state_dict(), save_model_path)
-    status_update({
-        "task": "saving_model_to_file",
-        "action": "finish"
-    })
+    status_update({"task": "saving_model_to_file", "action": "finish"})
 
-    status_update({
-        "task": "saving_model",
-        "action": "finish"
-    })
+    status_update({"task": "saving_model", "action": "finish"})
 
 def stylize(args):
-    status_update({
-        "task": "stylization_setup",
-        "action": "start"
-    })
+    status_update({"task": "stylization_setup", "action": "start"})
 
-    status_update({
-        "task": "torch_device_creation",
-        "action": "start"
-    })
+    status_update({"task": "torch_device_creation", "action": "start"})
     device = torch.device("cuda" if args.cuda else "cpu")
-    status_update({
-        "task": "torch_device_creation",
-        "action": "finish"
-    })
+    status_update({"task": "torch_device_creation", "action": "finish"})
 
-    status_update({
-        "task": "loading_content_image",
-        "action": "start"
-    })
+    status_update({"task": "loading_content_image", "action": "start"})
     content_image = utils.load_image(args.content_image, scale=args.content_scale)
     
-    status_update({
-        "task": "transform_creation",
-        "action": "start"
-    })
+    status_update({"task": "transform_creation", "action": "start"})
     content_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Lambda(lambda x: x.mul(255))
     ])
     content_image = content_transform(content_image)
-    status_update({
-        "task": "transform_creation",
-        "action": "finish"
-    })
+    status_update({"task": "transform_creation", "action": "finish"})
 
-    status_update({
-        "task": "unsqueezing_transforms",
-        "action": "start"
-    })
+    status_update({"task": "unsqueezing_transforms", "action": "start"})
     content_image = content_image.unsqueeze(0).to(device)
-    status_update({
-        "task": "unsqueezing_transforms",
-        "action": "finish"
-    })
+    status_update({"task": "unsqueezing_transforms", "action": "finish"})
 
-    status_update({
-        "task": "loading_content_image",
-        "action": "finish"
-    })
+    status_update({"task": "loading_content_image", "action": "finish"})
 
     if args.model.endswith(".onnx"):
         output = stylize_onnx_caffe2(content_image, args)
     else:
-        status_update({
-            "task": "loading_model",
-            "action": "start"
-        })
+        status_update({"task": "loading_model", "action": "start"})
 
         with torch.no_grad():
             style_model = TransformerNet()
@@ -420,20 +255,11 @@ def stylize(args):
             style_model.load_state_dict(state_dict)
             style_model.to(device)
 
-            status_update({
-                "task": "loading_model",
-                "action": "finish"
-            })
+            status_update({"task": "loading_model", "action": "finish"})
 
-            status_update({
-                "task": "stylization_setup",
-                "action": "finish"
-            })
+            status_update({"task": "stylization_setup", "action": "finish"})
 
-            status_update({
-                "task": "stylizing",
-                "action": "start"
-            })
+            status_update({"task": "stylizing", "action": "start"})
 
             if args.export_onnx:
                 assert args.export_onnx.endswith(".onnx"), "Export model file should end with .onnx"
@@ -441,10 +267,7 @@ def stylize(args):
             else:
                 output = style_model(content_image).cpu()
 
-            status_update({
-                "task": "stylizing",
-                "action": "finish"
-            })
+            status_update({"task": "stylizing", "action": "finish"})
 
     utils.save_image(args.output_image, output[0])
 
@@ -468,10 +291,7 @@ def stylize_onnx_caffe2(content_image, args):
 
 
 def main(start_conds):
-    status_update({
-        "task": "neural_style",
-        "action": "start"
-    })
+    status_update({"task": "neural_style", "action": "start"})
 
     args = utils.InputArgs(json.loads(start_conds.args))
     if args.subcommand is None:
@@ -485,10 +305,7 @@ def main(start_conds):
     else:
         stylize(args)
     
-    status_update({
-        "task": "neural_style",
-        "action": "finish"
-    })
+    status_update({"task": "neural_style", "action": "finish"})
 
 
 if __name__ == "__main__":
