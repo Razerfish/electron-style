1a248880cd0494d6f844ccb3a33ab016c1d0965f
Alec Wilson <alec.wilson@baxter-academy.org>
2019-05-24 10:59:17 -0400

    Remove broken prestart

--- Tools/prestart.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-if (fs.existsSync("src\\bin\\neural_style\\neural_style.exe")) {
-    process.exit(0);
-} else {
-    console.error("neural_style.exe not found, recompile by running 'npm run compile'\n");
-    process.exit(1);
-}
